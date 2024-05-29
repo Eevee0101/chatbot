73c9a024cc9d071b806ac48ec9d5c65a3f6ddbf7
Eevee0101 <104732318+Eevee0101@users.noreply.github.com>
2024-05-29 19:24:07 +0100

    Update MessageInput.jsx

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,134 +1,54 @@
-import {
-	Flex,
-	Image,
-	Input,
-	InputGroup,
-	InputRightElement,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalHeader,
-	ModalOverlay,
-	Spinner,
-	useDisclosure,
-};
-import { useRef, useState } from "react";
-import { IoSendSharp } from "react-icons/io5";
-import useShowToast from "../hooks/useShowToast";
-import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { BsFillImageFill } from "react-icons/bs";
-import usePreviewImg from "../hooks/usePreviewImg";
+import { useState } from "react";
 import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage";
 
-const MessageInput = ({ setMessages }) => {
-	const [messageText, setMessageText] = useState("");
-	const showToast = useShowToast();
-	const selectedConversation = useRecoilValue(selectedConversationAtom);
-	const setConversations = useSetRecoilState(conversationsAtom);
-	const imageRef = useRef(null);
-	const { onClose } = useDisclosure();
-	const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
-	const [isSending, setIsSending] = useState(false);
+const MessageInput = () => {
+	const [message, setMessage] = useState("");
+	const { loading, sendMessage } = useSendMessage();
 
-	const handleSendMessage = async (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (!messageText && !imgUrl) return;
-		if (isSending) return;
-
-		setIsSending(true);
-
-		try {
-			const res = await fetch("/api/messages", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					message: messageText,
-					recipientId: selectedConversation.userId,
-					img: imgUrl,
-				}),
-			});
-			const data = await res.json();
-			if (data.error) {
-				showToast("Error", data.error, "error");
-				return;
-			}
-			console.log(data);
-			setMessages((messages) => [...messages, data]);
-
-			setConversations((prevConvs) => {
-				const updatedConversations = prevConvs.map((conversation) => {
-					if (conversation._id === selectedConversation._id) {
-						return {
-							...conversation,
-							lastMessage: {
-								text: messageText,
-								sender: data.sender,
-							},
-						};
-					}
-					return conversation;
-				});
-				return updatedConversations;
-			});
-			setMessageText("");
-			setImgUrl("");
-		} catch (error) {
-			showToast("Error", error.message, "error");
-		} finally {
-			setIsSending(false);
-		}
+		if (!message) return;
+		await sendMessage(message);
+		setMessage("");
 	};
+
 	return (
-		<Flex gap={2} alignItems={"center"}>
-			<form onSubmit={handleSendMessage} style={{ flex: 95 }}>
-				<InputGroup>
-					<Input
-						w={"full"}
-						placeholder='Type a message'
-						onChange={(e) => setMessageText(e.target.value)}
-						value={messageText}
-					/>
-					<InputRightElement onClick={handleSendMessage} cursor={"pointer"}>
-						<IoSendSharp />
-					</InputRightElement>
-				</InputGroup>
-			</form>
-			<Flex flex={5} cursor={"pointer"}>
-				<BsFillImageFill size={20} onClick={() => imageRef.current.click()} />
-				<Input type={"file"} hidden ref={imageRef} onChange={handleImageChange} />
-			</Flex>
-			<Modal
-				isOpen={imgUrl}
-				onClose={() => {
-					onClose();
-					setImgUrl("");
-				}}
-			>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader></ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<Flex mt={5} w={"full"}>
-							<Image src={imgUrl} />
-						</Flex>
-						<Flex justifyContent={"flex-end"} my={2}>
-							{!isSending ? (
-								<IoSendSharp size={24} cursor={"pointer"} onClick={handleSendMessage} />
-							) : (
-								<Spinner size={"md"} />
-							)}
-						</Flex>
-					</ModalBody>
-				</ModalContent>
-			</Modal>
-		</Flex>
+		<form className='px-4 my-3' onSubmit={handleSubmit}>
+			<div className='w-full relative'>
+				<input
+					type='text'
+					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+					placeholder='Send a message'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+					{loading ? <div className='loading loading-spinner'></div> : <BsSend />}
+				</button>
+			</div>
+		</form>
 	);
 };
-
 export default MessageInput;
+
+// STARTER CODE SNIPPET
+// import { BsSend } from "react-icons/bs";
+
+// const MessageInput = () => {
+// 	return (
+// 		<form className='px-4 my-3'>
+// 			<div className='w-full'>
+// 				<input
+// 					type='text'
+// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+// 					placeholder='Send a message'
+// 				/>
+// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+// 					<BsSend />
+// 				</button>
+// 			</div>
+// 		</form>
+// 	);
+// };
+// export default MessageInput;
